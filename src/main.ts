60e5623e735731bd1f8e31c0b6a4ccac0883dccc
phuongtransptin <52156936+phuongtransptin@users.noreply.github.com>
2024-01-26 09:55:01 +0700

    fix-build

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,12 @@ const _supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4eGx4cnR4c2RjbGNidWpkeGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYwODY4MzMsImV4cCI6MjAyMTY2MjgzM30.lNfwkThtnyZUmmPGD2lQAdx5mpX4ZkuFns7239WqsFA"
 );
 
-console.log("👉 Start get all table 10k record...");
+const connectSupabase = async () => {
+  console.log("👉 Start get all table 10k record...");
 
-const getAllTable = await _supabase.from("my_table").select("*");
+  const getAllTable = await _supabase.from("my_table").select("*");
 
-console.log("✅ Finish get all table 10k record!!", getAllTable);
+  console.log("✅ Finish get all table 10k record!!", getAllTable);
+};
+
+connectSupabase();
