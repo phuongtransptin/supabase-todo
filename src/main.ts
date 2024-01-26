86139c6c560f4fe9a23387578841cff32a9217a5
phuongtransptin <52156936+phuongtransptin@users.noreply.github.com>
2024-01-26 11:06:51 +0700

    fix-bug

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,10 @@ createApp(App).mount("#app");
 /**
  * Keeps track of if the user is logged in or out and will update userSession state accordingly.
  */
+
+// Placing it here or any other `.ts` file works
+declare module "@vue/runtime-core" {
+  export interface ComponentCustomProperties {
+    $goto: any;
+  }
+}
--- src/typings/vue-shims.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.vue";
-// declare module "*.vue" {
-//   import Vue, { defineComponent } from "vue";
-
-//   const Component: typeof defineComponent;
-
-//   export { Vue, defineComponent };
-// }
